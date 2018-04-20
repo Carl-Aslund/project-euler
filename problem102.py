ebd398c2df0a501561862101259ec1a824f78e8d
Carl Aslund <caslund@g.hmc.edu>
2018-04-20 10:15:23 -0700

    Add a solution to Problem 102

---
+++ problem102.py
@@ -0,0 +1,11 @@
+result = 0
+
+for line in open('triangles_102.txt'):
+    ax, ay, bx, by, cx, cy = map(int, line.split(','))
+    a = ax*by - ay*bx > 0
+    b = bx*cy - by*cx > 0
+    c = cx*ay - cy*ax > 0
+
+    result += a==b==c
+
+print(result) # 228
